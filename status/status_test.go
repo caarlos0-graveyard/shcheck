5cd4f322b605c80ee547d1e783064f8eb5bfd156
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-06-24 18:49:21 -0300

    fix test

--- status/status_test.go
+++ status/status_test.go
@@ -2,16 +2,11 @@ package status
 
 import (
 	"testing"
-
-	"time"
 )
 
 func TestAllStatuses(t *testing.T) {
 	var file = "/path/to/my/file.sh"
-	Info(file)
-	time.Sleep(1 * time.Second)
+	Ignore(file)
 	Success(file)
-	Info(file)
-	time.Sleep(1 * time.Second)
 	Fail(file)
 }
