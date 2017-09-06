06b67667e9ef7eb9ba5c1a5963af409e57ea268d
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-09-06 17:21:51 -0300

    kingpin

--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "github.com/caarlos0/shcheck/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/caarlos0/shcheck/sh"
+	"github.com/caarlos0/shcheck/status"
+	zglob "github.com/mattn/go-zglob"
+)
+
+var (
+	version            = "master"
+	app                = kingpin.New("shcheck", "shcheck validates your scripts against both shellcheck and shfmt")
+	ignoredFiles       = app.Flag("ignore", "ignore files or folders").HintOptions("folder/**/*", "*.bash").Short('i').Strings()
+	shellcheckExcludes = app.Flag("shellcheck-exclude", "exclude some shellcheck checks").HintOptions("SC1090", "SC1004").Short('e').Strings()
+)
 
 func main() {
-	cmd.Execute()
+	app.Version("shfmt version " + version)
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+	var fail bool
+	files, err := zglob.Glob(`**/*.*sh`)
+	kingpin.FatalIfError(err, "fail to find all shell files")
+
+	for _, file := range files {
+		if err := check(file); err != nil {
+			fail = true
+		}
+	}
+	if fail {
+		kingpin.Fatalf("\nsome checks failed. check output above.\n")
+	}
+}
+
+func check(file string) error {
+	if ignore(*ignoredFiles, file) {
+		status.Ignore(file)
+		return nil
+	}
+	var options = sh.Options{
+		Shellcheck: sh.ShellcheckOptions{
+			Exclude: *shellcheckExcludes,
+		},
+	}
+	var errors []error
+	for _, check := range sh.Checkers(options) {
+		if err := check.Check(file); err != nil {
+			errors = append(errors, err)
+		}
+	}
+	if len(errors) == 0 {
+		status.Success(file)
+		return nil
+	}
+	status.Fail(file)
+	for _, err := range errors {
+		fmt.Println(err)
+	}
+	return fmt.Errorf("check failed")
+}
+
+func ignore(patterns []string, file string) bool {
+	for _, pattern := range patterns {
+		if ok, err := zglob.Match(pattern, file); ok && err == nil {
+			return true
+		}
+	}
+	return false
 }
